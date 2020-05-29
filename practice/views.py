c4d2d8be9d6d4de9108eeb55dd65b730407a4477
johnny <johnkseongin@gmail.com>
2020-05-30 02:48:34 +0900

    new

--- practice/views.py
+++ practice/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world. You are at practice index.")
+    return HttpResponse("Hello world. You are at practice index. new")
